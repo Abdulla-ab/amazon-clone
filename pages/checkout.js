0f72a7b800613098ca971bb9da4465f2ebd95099
Abdulla-ab <2019cs001@stu.ucsc.lk>
2022-06-13 23:26:46 +0530

    redux

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Image from 'next/Image'
+import { useSelector } from 'react-redux'
+import { selectItems } from '../slices/basketSlice'
+import CheckoutProduct from '../components/CheckoutProduct'
 
 function Checkout() {
+
+  const items = useSelector(selectItems)
+
   return (
     <div className='bg-gray-100'>
       <Header />
@@ -14,7 +20,22 @@ function Checkout() {
           <Image src="https://links.papareact.com/ikj" width={1020} height={250} objectFit="contain"/>
 
           <div className='flex flex-col p-5 space-y-30 bg-white'>
-            <h1 className='text-3xl border-b pb-4'>Shopping Basket</h1>
+            <h1 className='text-3xl border-b pb-4'>{items.length == 0 ? 'Your Shopping Basket is empty' : 'Your Shopping Basket'}</h1>
+
+            {items.map((item, i) => (
+              <CheckoutProduct 
+                key={i}
+                id={item.id}
+                title={item.title}
+                price={item.price}
+                rating={item.rating}
+                image={item.image}
+                description={item.description}
+                category={item.category}
+                hasPrime={item.hasPrime}
+              />
+            ))}
+
           </div>
 
         </div>
--- slices/basketSlice.js
+++ slices/basketSlice.js
@@ -12,11 +12,27 @@ export const basketSlice = createSlice({
         addToBasket: (state, action) => {
             state.items = [...state.items, action.payload];
         },
-        removeFromBasket: (state, action) => {},
-    },
+        removeFromBasket: (state, action) => {
+            const index = state.items.findIndex(
+                (basketItem) => basketItem.id === action.payload.id
+            );
+        
+
+            let newBasket = [...state.items];
+            
+            if(index >= 0){
+                //  The item index in the basket ... remove it
+                newBasket.splice(index, 1);
+            }else{
+                console.warn('Cant remove product (id : ${action.payload.id}) as its not in the basket');
+            }
+
+            state.items = newBasket;
+        },
+    }
 });
 
-export const { addToBasket, removeFromBasket } = basketSlice.actions;
+export const { addToBasket, removeFromBasket } = basketSlice.actions; 
 
 // Selectors - This is how we pull information from the global store slice
 export const selectItems = state => state.basket.items;
